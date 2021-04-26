d4bec7e352ff8c5d5d363703da857b5ebf8444a5
Samson N <samnzumg@gmail.com>
2021-04-26 09:54:42 +0200

    nouvelle collecte

---
+++ src/collectors/collectors_dataworld.py
@@ -0,0 +1,19 @@
+import datadotworld as dw
+import csv
+import os.path
+
+# telechargement de la page
+dataset = dw.load_dataset('fivethirtyeight/2020-general-election-forecast')
+
+# creation de notre fichier local
+if not os.path.isfile('presidential_state_toplines_2020.csv'):
+        f = open('presidential_state_toplines_2020.csv','w')
+        f.close()
+
+with open('presidential_state_toplines_2020.csv','w') as w:
+    relation = csv.writer(w, delimiter=',')
+    relation.writerow(dataset.tables['presidential_state_toplines_2020'][0].keys())
+    for i in range(len(dataset.tables['presidential_state_toplines_2020'])):
+        relation.writerow(dataset.tables['presidential_state_toplines_2020'][i].values())
+
+
